34aa5abfebfe8a675639c3d3605d13a680049d4f
SpaceWinner <38380570+SpaceWinner@users.noreply.github.com>
2018-05-06 22:52:47 +0500

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -4,10 +4,13 @@ import sys
 import codecs
 import threading
 import re
+import time
+import os
+import signal
+from multiprocessing.pool import ThreadPool
 
 _tcp_timeout = 2
 _udp_timeout = 2
-_message_format = '{}: {} is a {} port'
 _udp_payloads = {
     'dns': '000000000001000000000000095f7365727669636573075f646e732d7364045f756470056c6f63616c00000c0001',
 
@@ -33,18 +36,42 @@ _udp_payloads = {
     'time': '000000000001000000000000095f7365727669636573075f646e732d7364045f756470056c6f63616c00000c0001',
 }
 
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def is_ip_address(string):
     return bool(re.match(r'^(\d+\.){3}\d+$', string))
 
 def scan_port(args):
-    for port in range(input_data['start'], input_data['end']):
-        threading.Thread(target=scan_TCP, args=(args['host'], port)).start()
-        local = threading.local().__dict__
-        for proto in _udp_payloads:
-            params = args['host'], port, proto, local
-            threading.Thread(target=scan_UDP, args=params).start()
+        try:
+            signal.signal(signal.SIGINT, lambda *_: quit())
+            pool = ThreadPool(1000)
+            jobs = []
+            local = threading.local().__dict__
+            start, end = input_data['start'], input_data['end']
+            for port in range(start, end):
+                jobs.append(pool.apply_async(scan_TCP, [args['host'], port, local]))
+                for proto in _udp_payloads:
+                    jobs.append(pool.apply_async(scan_UDP, [args['host'], port, proto, local]))
+            pool.close()
+            while not all(x.ready() for x in jobs):
+                percent = round(100 * len([x for x in jobs if x.ready()]) / len(jobs), 2)
+                print_with_progress(local, percent)
+                time.sleep(1)
+            print_with_progress(local, 100)
+        except KeyboardInterrupt:
+            quit()
 
-def scan_TCP(host, port):
+def print_with_progress(local, percent):
+    cls()
+    print(f'Progress: {percent}% complete')
+    for key, guess_proto in local.items():
+        transport_proto, port = key
+        print(f'Discovered {port} port on {transport_proto}', f'({guess_proto})' if guess_proto else '')
+
+def scan_TCP(host, port, local):
+    if ('tcp', port) in local:
+        return
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.settimeout(_tcp_timeout)
@@ -52,23 +79,23 @@ def scan_TCP(host, port):
         except (socket.timeout, socket.error):
             pass
         else:
-            print(_message_format.format(host, port, 'TCP'))
+            local[('tcp', port)] = None
 
 
 def scan_UDP(host, port, proto, local):
+    if ('udp', port) in local:
+        return
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    host_name = host
     if not is_ip_address(host):
         host = socket.gethostbyaddr(host)[2][0]
     sock.settimeout(_udp_timeout)
     payload = _udp_payloads[proto]
-    for i in range(1, 10):
+    for _ in range(1, 10):
         sock.sendto(codecs.decode(payload, 'hex'), (host, port))
     try:
         _, addr = sock.recvfrom(1024)
-        if addr == (host, port) and not port in local:
-            local[port] = proto
-            print(_message_format.format(host_name, port, 'UDP'))
+        if addr == (host, port):
+            local[('udp', port)] = None
     except socket.timeout:
         pass
 
